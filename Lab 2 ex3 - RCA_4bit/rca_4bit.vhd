4414da2932ba3de4b0ac10d24318f78c4ad974bf
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-02-12 06:57:30 -0600

    5 bit multiplier VHDL

--- Lab 2 ex3 - RCA_4bit/rca_4bit.vhd
+++ Lab 2 ex3 - RCA_4bit/rca_4bit.vhd
@@ -1,3 +1,40 @@
+--library IEEE;
+--use IEEE.STD_LOGIC_1164.ALL;
+
+--entity rca_4bit is
+
+--port ( A : in std_logic_vector (3 downto 0);
+	--	 B : in std_logic_vector (3 downto 0);
+	--	 Cin : in std_logic;
+	--	 S : out std_logic_vector (3 downto 0);
+	--	 Cout : out std_logic);
+--end rca_4bit;
+
+--architecture behavioral of rca_4bit is
+
+	 
+-- Full Adder VHDL Code Component Decalaration
+--component full_adder
+--port ( A : in std_logic;
+	   -- B : in std_logic;
+	   -- Cin : in std_logic;
+	   -- S : out std_logic;
+	   -- Cout : out std_logic);
+--end component;
+
+	-- Intermediate Carry declaration
+	--signal c1,c2,c3: std_logic;
+
+	--begin
+
+	-- Port Mapping Full Adder 4 times
+--FA1: full_adder port map( A(0), B(0), Cin, S(0), c1);
+--FA2: full_adder port map( A(1), B(1), c1, S(1), c2);
+--FA3: full_adder port map( A(2), B(2), c2, S(2), c3);
+--FA4: full_adder port map( A(3), B(3), c3, S(3), Cout);
+
+--end behavioral;
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -8,29 +45,34 @@ port ( A : in std_logic_vector (3 downto 0);
 		 Cin : in std_logic;
 		 S : out std_logic_vector (3 downto 0);
 		 Cout : out std_logic);
+		 
 end rca_4bit;
 
 architecture behavioral of rca_4bit is
 
 	 
 -- Full Adder VHDL Code Component Decalaration
-component full_adder
-port ( A : in std_logic;
-	    B : in std_logic;
-	    Cin : in std_logic;
-	    S : out std_logic;
-	    Cout : out std_logic);
-end component;
+		component full_adder 
+
+				port ( A : in std_logic;
+						 B : in std_logic;
+						 Cin : in std_logic;
+						 S : out std_logic;
+						 Cout : out std_logic);
+		 
+				end component;
 
 	-- Intermediate Carry declaration
-	signal c1,c2,c3: std_logic;
+	--signal c1,c2,c3: std_logic;
+	
+	signal Crazy: std_logic_vector(3 downto 1);
 
 	begin
 
 	-- Port Mapping Full Adder 4 times
-FA1: full_adder port map( A(0), B(0), Cin, S(0), c1);
-FA2: full_adder port map( A(1), B(1), c1, S(1), c2);
-FA3: full_adder port map( A(2), B(2), c2, S(2), c3);
-FA4: full_adder port map( A(3), B(3), c3, S(3), Cout);
+FA1: full_adder port map( A(0), B(0), Cin, S(0), Crazy(1));
+FA2: full_adder port map( A(1), B(1), Crazy(1), S(1), Crazy(2));
+FA3: full_adder port map( A(2), B(2), Crazy(2), S(2), Crazy(3));
+FA4: full_adder port map( A(3), B(3), Crazy(3), S(3), Cout);
 
 end behavioral;
--- Lab 2 ex3 - multiplier/array_multi_5bit.vhd
+++ Lab 2 ex3 - multiplier/array_multi_5bit.vhd
@@ -2,69 +2,47 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity array_multi_5bit is
-port(X4 : in std_logic;
-     X3 : in std_logic;
-	  X2 : in std_logic;
-	   X1 : in std_logic;
-	   X0 : in std_logic;
-		Y4 : in std_logic;
-		Y3 : in std_logic;
-		Y2 : in std_logic;
-	   Y1 : in std_logic;
-	   Y0 : in std_logic;
-	  --Cin:
-	  Cout: out std_logic_vector(19 downto 0);
-	  P: out std_logic_vector(9 downto 0)
-     );
+port(
+		A: in std_logic_vector(4 downto 0);
+		B: in std_logic_vector(4 downto 0);
+		P: out std_logic_vector(9 downto 0)
+		--Cout: out std_logic_vector(19 downto 1)
+      );
 
 end entity array_multi_5bit;
 
-architecture dataflow of array_multi_5bit is
+architecture structure of array_multi_5bit is
 
-signal nocarry : std_logic;
-
-
-
-
-
-component rca_4bit is
-
-port ( A3 : in std_logic;
-		 A2 : in std_logic;
-		 A1 : in std_logic;
-	    A0 : in std_logic;	
-		 B3 : in std_logic;
-		 B2 : in std_logic;
-		 B1 : in std_logic;
-		 B0 : in std_logic;
-		 Cin : in std_logic;
-		 S : out std_logic;
-		 Cout : out std_logic);
-		 
-end component;
+signal noCarry: std_logic;
+signal Cinternal: std_logic_vector(19 downto 0);
+signal Sums : std_logic_vector(19 downto 0);
+--signal Cinternal_level_1: std_logic_vector(3 downto 1);
 
 
+
+--signal Sum_level_0 : std_logic_vector(3 downto 0);
+--signal Sum_level_1 : std_logic_vector(3 downto 0);
 
 
 begin
 
-nocarry <= '0';
+noCarry <= '0';
 
-Level1: rca_4bit port map(X4 AND Y0, X3 AND Y0, X2 AND Y0, X1 AND Y0, X3 AND Y1, X2 AND Y1, X1 AND Y1, X0 AND Y1, nocarry, P1, C0);
+level_0: entity work.rca_4bit(behavioral)
+port map( 
+B(0) => A(1) AND B(0), A(0) => A(0) AND B(1), B(1) => A(2) AND B(0), A(1) => A(1) AND B(1), B(2) => A(3) AND B(0), A(2) => A(2) AND B(1), B(3) => A(4) AND B(0), A(3) => A(3) AND B(1), 
+Cin => noCarry, 
+S(0) => Sums(0), S(1) => Sums(1), S(2) => Sums(2), S(3) => Sums(3), 
+Crazy(1) => Cinternal(0), Crazy(2) => Cinternal(1), Crazy(3) => Cinternal(2), Cout => Cinternal(3) ); 
 
---Level1: entity work.rca_4bit(behavioral)
---port map(s0 => s0, s1 => s1, D0 => D0, D1 => D1, D2 => D2, D3 => D3, y => x1); 
+--level_1: entity work.rca_4bit(behavioral)
+--port map( B(0) => Sums(1), A(0) => A(0) AND B(2), B(1) => Sums(2), A(1) => A(1) AND B(2), B(2) => Sums(3), A(2) => A(2) AND B(2), B(3) => A(4) AND B(1), A(3) => A(3) AND B(2), Cin => Cinternal(0), S(0) => Sums(4), S(1) => Sums(5), S(2) => Sums(6), S(3) => Sums(7), C(1) => Cinternal(4), C(2) => Cinternal(5), C(3) => Cinternal(6), Cout => Cinternal(7) ); 
 
---Level3: entity work.rca_4bit(behavioral)
---port map(s0 => s0, s1 => s1, D0 => D0, D1 => D1, D2 => D2, D3 => D3, y => x1); 
+--level3:
+--level4:
+--level5:
 
---Level4: entity work.rca_4bit(behavioral)
---port map(s0 => s0, s1 => s1, D0 => D0, D1 => D1, D2 => D2, D3 => D3, y => x1); 
+--P(0) <= A(0) AND B(0); --value of product 0
+--P(1) <= Sums(0);
 
---Level5: entity work.rca_4bit(behavioral)
---port map(s0 => s0, s1 => s1, D0 => D0, D1 => D1, D2 => D2, D3 => D3, y => x1); 
-
-
-P(0) <= X(0) AND Y(0);
-
-end architecture dataflow;
+end architecture structure;
