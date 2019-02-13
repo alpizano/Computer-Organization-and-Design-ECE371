334efb629ca7ec52afb4f8f20ad98268322b3461
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-02-12 21:05:00 -0600

    more VHDL code

---
+++ Lab 2 ex4 - rotator/rotator_4bit.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity rotator_4bit is
+port(A: in std_logic;
+	  B: out std_logic
+);
+
+
+end entity rotator_4bit;
+
+architecture dataflow of rotator_4bit is
+
+
+begin
+
+
+end architecture dataflow;
