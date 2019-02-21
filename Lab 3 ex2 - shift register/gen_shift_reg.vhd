a903ab80b4ea972e93a5a64445f03fb0761d8847
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-02-21 03:11:49 -0600

    finished generic shifter

--- Lab 3 ex2 - shift register/gen_shift_reg.vhd
+++ Lab 3 ex2 - shift register/gen_shift_reg.vhd
@@ -9,28 +9,32 @@ entity gen_shift_reg is
 		--rst	: in std_logic;
 		--en	   : in std_logic; 
 		CLK	: in std_logic; 
-		Sin	: in std_logic_vector(N-1 downto 0); --Sin
-		--Sout  : out std_logic;
-		Sout	   : out std_logic_vector(N-1 downto 0) -- Sout
-
+		Sin	: in std_logic; --D
+		Sout	 : out std_logic -- Q
 		); 
 end entity;
 
 architecture structure of gen_shift_reg is
 
 
-signal Qinternal : std_logic_vector ---- QN-1:0
+signal Qinternal : std_logic_vector(N downto 0); ---- QN-1:0
 
+ 
 
 begin
+
+Qinternal(0) <= Sin;
 	
-  d_ff_stages: for k in 0 to N-1 generate 
+  d_ff_stages: for i in 0 to N-1 generate 
   
 	  d_ff: entity work.d_ff(behavioral)
-		port map(CLK => CLK, D => Sin(k), Q => Sout(k));
+	  
+		port map(CLK => CLK, D => Qinternal(i), Q => Qinternal(i+1));
 	
 	end generate d_ff_stages;
 	
-	--Sout <= Q(0);
+	
+	
+	Sout <= Qinternal(N-1);
 	
 end architecture structure;
