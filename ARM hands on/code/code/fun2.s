d419bdf2eecbc3fbe7238e1b1e1ec97d7685f016
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-05-03 01:19:33 -0500

    coded assembly for swapping numbers

--- ARM hands on/code/code/fun2.s
+++ ARM hands on/code/code/fun2.s
@@ -5,10 +5,10 @@
 
 /* Swapping numbers */
 
-		MOV R3,R0      @ Move arg1 of function to R3, i.e. 4
-		MOV R4,R1      @ Move arg2 of function to R4, i.e. 3
-swap:   ADD R5, R3, R4 @ R5 = R3 + R4, i.e. 7 = 4 + 3 
-        MOV R4, R3     @ Move R3 to R4, i.e. R4 = 4
-		SUB R6, R5, R4 @ R6 = R5 - R4, i.e. R6 = 7 - 4 = 3
-		BX LR
+/* Swapping numbers */
 
+swap:   LDR R3,[R0]
+        LDR R4,[R1] 
+        STR R3,[R1]
+        STR R4,[R0]  
+        BX LR
